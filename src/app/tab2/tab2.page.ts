1e1e92e31123c5c4838fd6a1bd885fca36edec5b
Tanvir Hossain <tanvirjewel@live.com>
2020-04-10 13:27:01 +0600

    Code added.

---
+++ src/app/tab2/tab2.page.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { AppService } from '../app.service';
+import { LoadingController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+export class Tab2Page {
+
+  countries: any;
+  data: any;
+  constructor(private appService: AppService,
+    public loadingController: LoadingController) { }
+
+  ionViewDidEnter() {
+    this.getData();
+  }
+
+  search() {
+    const searchbar = document.querySelector('ion-searchbar');
+    const items = Array.from(document.querySelector('ion-list').children);
+
+    searchbar.addEventListener('ionInput', handleInput);
+
+    function handleInput(event) {
+      const query = event.target.value.toLowerCase();
+      requestAnimationFrame(() => {
+        items.forEach((item) => {
+          const shouldShow = item.textContent.toLowerCase().indexOf(query) > -1;
+          shouldShow ? item.removeAttribute('hidden') : item.setAttribute('hidden', 'hidden');
+        });
+      });
+    }
+  }
+
+  async getData() {
+    const loading = await this.loadingController.create({
+      message: 'Loading...',
+      duration: 10000
+    });
+    await loading.present();
+    this.appService.getData().subscribe(res => {
+      this.data = res;
+      this.countries = this.data['Countries'];
+      this.countries.sort((a, b) => (a.TotalConfirmed < b.TotalConfirmed) ? 1 : -1);
+      loading.dismiss();
+      setTimeout(() => {
+        this.search();
+      }, 200);
+    }, (error) => {
+      loading.dismiss();
+    });
+  }
+
+}
